00598a80a3b3f076a61ffaa178f92be998504e99
dipeshjain1906-code <dipeshjain1906@gmail.com>
2025-12-06 02:20:07 +0530

    Lecture-3

--- 03_type-conversion.js
+++ 03_type-conversion.js
@@ -1,18 +1,61 @@
-let score = "33ac"
-score = null;
-score = true;
-console.log(typeof score)
+// let score = "33ac"
+// score = null;
+// score = true;
+// console.log(typeof score)
 
-let temp = Number(score)
-console.log(typeof temp)
-console.log(temp)
+// let temp = Number(score)
+// console.log(typeof temp)
+// console.log(temp)
 
-// 33 -> 33
-// "33abc" -> NaN
-// true -> 1
+// // 33 -> 33
+// // "33abc" -> NaN
+// // true -> 1
 
-let loggedin 
-let booleanlog = Boolean(loggedin)
-console.log(booleanlog)
-// 1-> true 0-> false
-// "",null = false
+// let loggedin 
+// let booleanlog = Boolean(loggedin)
+// console.log(booleanlog)
+// // 1-> true 0-> false
+// // "",null = false
+
+
+/********************  Operations  ********************/
+
+// console.log(2+2)
+// console.log(2**4)//2 raised to power 4
+
+// console.log(1 + 2 + "2")
+// console.log("1" + 2 + 2)
+
+
+// // 2+2
+// // 2-2
+// // 2*2
+// // 2**3
+// // 2/3
+// // 2%3
+
+// let num1,num2,num3
+// num1 = num2 = num3 = 4
+
+/************************** Comparisons ******************/
+
+// console.log(2>1)
+// console.log(2<1)
+// console.log(2 != 1)
+
+// console.log("2" < 1)
+
+
+// console.log(null > 0)
+// console.log(null == 0)
+// console.log(null >= 0)
+
+// // "==" can converts datatype
+// // "===" strict equalto also compares datatype
+
+// console.log("0" == 0)
+// console.log("0" === 0)
+// console.log(1 == "1")
+
+// let id = Symbol(true)
+// console.log(id)

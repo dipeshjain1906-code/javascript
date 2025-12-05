133fe856e8ff1b7a87b0ab9df90f07ac0f20582a
dipeshjain1906-code <dipeshjain1906@gmail.com>
2025-12-06 01:19:37 +0530

    Lecture-3

---
+++ 03_type-conversion.js
@@ -0,0 +1,18 @@
+let score = "33ac"
+score = null;
+score = true;
+console.log(typeof score)
+
+let temp = Number(score)
+console.log(typeof temp)
+console.log(temp)
+
+// 33 -> 33
+// "33abc" -> NaN
+// true -> 1
+
+let loggedin 
+let booleanlog = Boolean(loggedin)
+console.log(booleanlog)
+// 1-> true 0-> false
+// "",null = false

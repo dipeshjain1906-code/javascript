1d5c9d7bea8ce2dee32cc93f393f0207995eded1
dipeshjain1906-code <dipeshjain1906@gmail.com>
2025-12-06 01:06:53 +0530

    lecture-2

---
+++ 01_declaration_keywords.js
@@ -0,0 +1,16 @@
+const accountid = 132331
+let accountname = "Dipesh@google.com"
+var accounttype = "Savings account"
+accountCity = "Faridabad"
+
+
+// accountid =2
+accountname = "Dipu"
+accounttype = "Current";
+accountCity = "Jaipur";
+
+/* prefer not to use var ever in js 
+    if not given a value then the variable is undefined
+*/
+
+console.table([accountid,accountname,accounttype,accountCity])
---
+++ 02_datatypes.js
@@ -0,0 +1,18 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(2+2)  we are using nodejs not browser
+
+// number upto 2 to power 53
+// bigint
+// string ""
+// boolean => true/false
+// null => standalone value
+// undefined => not defined a value
+// symbol => unique
+
+//object
+
+console.log(typeof "Dipesh")
+console.log(typeof undefined)
+console.log(typeof null)
+console.log(typeof 4)
